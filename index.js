893a82fab3cc84c0df2ba818e5ad89b9050d1c22
minhazesiraz <minhazesiraz@gmail.com>
2024-06-01 23:01:09 +0600

    edited a posts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const moment = require('moment');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -14,13 +15,25 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://minhazesiraz:VFOLix62KT0t5DL2@minhazesiraz.vnljkwg.mongodb.net/?retryWrites=true&w=majority&appName=minhazesiraz";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//    serverApi: {
+//       version: ServerApiVersion.v1,
+//       strict: true,
+//       deprecationErrors: true,
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//    }
+// });
+
+/* Create a MongoClient with a MongoClientOptions object to set the Stable API version */
 const client = new MongoClient(uri, {
    serverApi: {
       version: ServerApiVersion.v1,
-      strict: true,
+      strict: false,
       deprecationErrors: true,
    }
 });
+
 
 async function run() {
    try {
@@ -76,8 +89,72 @@ async function run() {
       })
 
       /* Blogs */
+      /*
       app.get("/blogs", async (req, res) => {
          const result = await blogsGathering.find().toArray();
+         res.send(result);
+      })
+      */
+
+      /* Create indexes - strict: false */
+      await blogsGathering.createIndexes([
+         { key: { title: 'text', description: 'text' } },
+         /* Additional indexes can be created here */
+      ]);
+
+      app.get("/blogs", async (req, res) => {
+         const { page = 1, limit = 10, q = '' } = req.query;
+         const skip = (page - 1) * limit;
+         const query = q ? { $text: { $search: q.toLowerCase() } } : {};
+
+         try {
+            const totalBlogs = await blogsGathering.countDocuments(query);
+            const blogs = await blogsGathering.find(query)
+               .skip(skip)
+               .limit(parseInt(limit))
+               .toArray();
+
+            res.send({
+               blogs,
+               totalPages: Math.ceil(totalBlogs / limit),
+               currentPage: parseInt(page),
+            });
+         } catch (error) {
+            res.status(500).send({ message: error.message });
+         }
+      });
+
+      app.get("/blogs/posts/:pid", async (req, res) => {
+         const pid = req.params.pid;
+         const query = { _id: new ObjectId(pid) }
+         const result = await blogsGathering.findOne(query);
+         res.send(result);
+      })
+
+      app.post("/blogs/create-a-posts", async (req, res) => {
+         const job = req.body;
+         const result = await blogsGathering.insertOne(job);
+         res.send(result);
+      })
+
+      app.put("/blogs/edited-a-posts/:pid", async (req, res) => {
+         const blog = req.body;
+         const pid = req.params.pid;
+         console.log(pid);
+         console.log(blog);
+         const filter = { _id: new ObjectId(pid) };
+         const updateDoc = {
+            $set: {
+               title: blog.title,
+               description: blog.description,
+               image: blog.image,
+               category: blog.category,
+               author: blog.author,
+               authorImage: blog.authorImage,
+               date: moment().format('MMMM Do YYYY, h:mm:ss a')
+            }
+         }
+         const result = await blogsGathering.updateOne(filter, updateDoc);
          res.send(result);
       })
 
